4165de8accc48f2a2b179433e9c089acbd050577
nicol-hawkins <nicolchawkins@gmail.com>
2020-04-01 13:55:42 -0700

    nth-child teal bars working again

--- js/main.js
+++ js/main.js
@@ -1,68 +1,29 @@
 
+function render() {
+    fetch("./data/year_data.json")
+        .then(response => response.json())
+        .then(data => {
+            console.log("got the data!");
+            console.log(data);
 
-        function render() {
-            fetch("./data/year_data.json")
-                .then(response => response.json())
-                .then(data => {
-                    console.log("got the data!");
-                    console.log(data);
+            let chart = document.querySelector("#results");
 
-                    for (let item in data) {
-                        console.log('Country: ', data[item].country)
-                        console.log('Years: ', data[item].years )   
-                    }
+            for (let item in data) {
+                console.log('Country: ', data[item].country)
+                console.log('Years: ', data[item].years)   
 
-                })
+                let bar = document.createElement("div");
+                let height = 70;
+                bar.classList.add("BarChart-Bar");
+                bar.style.height = height + "%";
+                chart.appendChild(bar);
 
-          
+            }
 
-            // for (let [key, value] of Object.entries(data)) {
-            //     console.log(`${key}: ${value}`);
-               
-            //     }
+        })
 
-
-
-
-
-
-
-            // let chart = document.querySelector('#results');
-            
-            // for (country in data);
-            // let key = document.write(country);
-            // let value = document.write(data[country]);
-            // console.log('Country: ', value);
-        
-            // for (let item in data) {
-            //     let bar = document.createElement("div");
-            //     let height = 70;
-            //     bar.classList.add("BarChart-Bar");
-            //     bar.style.height = height + "%";
-            //     chart.appendChild(bar);
-              
-            // }
-        }
-        render();
+    }
+    render();
         
 
 
-
-// function render() {
-//     let chart = document.querySelector('#results');
-    
-//     for (country in data);
-//     let key = document.write(country);
-//     let value = document.write(data[country]);
-//     console.log('Country: ', value);
-
-//     for (let item in data) {
-//         let bar = document.createElement("div");
-//         let height = 70;
-//         bar.classList.add("BarChart-Bar");
-//         bar.style.height = height + "%";
-//         chart.appendChild(bar);
-      
-//     }
-// }
-// render();
