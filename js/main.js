f1512813bbafae941413dd59c56bf3f0065b9c9a
nicol-hawkins <nicolchawkins@gmail.com>
2020-04-01 21:05:35 -0700

    console logged years from data

--- js/main.js
+++ js/main.js
@@ -7,15 +7,22 @@ function render() {
             console.log(data);
 
             let chart = document.querySelector("#results");
+           
 
             for (let item in data) {
                 console.log('Country: ', data[item].country)
-                console.log('Years: ', data[item].years)   
+                console.log('Data for Country: ', data[item].years)   
+                console.log("Years List: ,", Object.keys(data[item].years))
+
+               
+                let percent = data[item].years["2010"];
+                console.log("2010 percent: ", percent);
 
                 let bar = document.createElement("div");
                 let height = 70;
                 bar.classList.add("BarChart-Bar");
                 bar.style.height = height + "%";
+                bar.innerHTML = data[item].country + '<br>' + percent + " %"
                 chart.appendChild(bar);
 
             }
@@ -36,6 +43,7 @@ function render() {
                 console.log('Country Buttons: ', data[item].country)
                 let button = document.createElement("button");
                 button.classList.add('Country-Button');
+                button.innerHTML = data[item].country;
                 checkbox.appendChild(button);
 
             }
@@ -44,6 +52,42 @@ function render() {
       
     }
     countryButtons();
+
+
+    function selectYear(){
+        fetch("./data/year_data.json")
+            .then(response => response.json())
+            .then(data => {
+        
+                let selectYear = document.querySelector('#select');
+                // console.log(Object.keys(data[item].years))
+                for (let item in data) {
+                    console.log("Years List: ,", Object.keys(data[item].years))      
+                    let year_choice = document.createElement("option");
+                    year_choice.classList.add('year-choice');
+                    year_choice.innerHTML = Object.keys(data[item].years)
+                    selectYear.appendChild(year_choice);
+
+                    // let years = Object.keys(data[item].years);
+                    // for (let year in years) {
+                    // console.log('Years: ', year)
+
+
+                    // let selectedYearOption = document.querySelector('#select :checked');
+                    // console.log('Year:', selectedYearOption);
+                    // let year = selectedYearOption.value;
+                    // console.log('Getting comment text', year);
+                    // newCommentDiv.style.color = colorText;
+                }
+
+                
+
+                
+
+               
+             });
+    }
+    selectYear();
         
 
 
